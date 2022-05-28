46312e2dded44e04a33f69d4da460f3ffe7cba19
lalka106 <edik66948@gmail.com>
2022-05-28 10:03:39 +0300

    a little bit change docx

--- word1.php
+++ word1.php
@@ -16,35 +16,56 @@ $cellStyle = array(
     'borderTopSize' =>3,
     'borderRightSize' =>3,
     'borderBottomSize' =>3,
-    'borderLeftSize' =>3
+    'borderLeftSize' =>3,
+    'align' => 'center'
 );
 $textStyle = array(
     'align'=>'center'
 );
 $section = $phpWord->addSection();
+$section->addText('ЧУП "Завод СВТ"',array('bold'=>true,'align'=>'center'));
+$table = $section->addTable('Ostatki');
+$table->addRow(500);
+$table->addCell(3000,$cellStyle)->addText('Дата составления',$textStyle);
+$table->addRow(500);
+$cell = $table->addCell(3000,$cellStyle);
+$cell->addText(date("j.n.Y"));
 $section->addText('Ведомость наличия товаров',array('bold'=>true,'align'=>'center'));
-$firstRowStyle = array('bgColor' => '66BBFF');
+$firstRowStyle = array('bgColor' => '66BBFF','bold'=>true);
 $phpWord->addTableStyle('Ostatki', $tableStyle, $firstRowStyle);
 $table = $section->addTable('Ostatki');
 $table->addRow(500);
 $table->addCell(3000,$cellStyle)->addText('Наименование товара',$textStyle);
-$table->addCell(3000,$cellStyle)->addText('Количество на складе',$textStyle);
+$table->addCell(500,$cellStyle)->addText('Код товара',$textStyle);
+$table->addCell(500,$cellStyle)->addText('Количество на складе, шт.',$textStyle);
+$table->addCell(3000,$cellStyle)->addText('Учетная стоимость, руб. коп.',$textStyle);
 for ($i=0;$i<count($products);$i++){
     $table->addRow(500);
     $name = $products[$i]['name'];
+    $id = $products[$i]['id'];
     $count = $products[$i]['count'];
+    $price = $products[$i]['price'];
     $cell = $table->addCell(3000,$cellStyle);
     $cell->addText($name);
-    $cell = $table->addCell(3000,$cellStyle);
+    $cell = $table->addCell(500,$cellStyle);
+    $cell->addText($id);
+    $cell = $table->addCell(500,$cellStyle);
     $cell->addText($count);
+    $cell = $table->addCell(3000,$cellStyle);
+    $cell->addText($price);
+
 //    $document->setValue("name",$name);
 //    $document->setValue("count",$count);
 //    $section = $document->cloneRowAndSetValues("name",$name);
 
 }
 
-$section->addText('Должность - ');
-$section->addText('Подпись - ');
+$table->addRow(500);
+$table->addCell(3000)->addText('Должность_________________',$textStyle);
+$table->addCell(3000)->addText();
+$table->addCell(3000)->addText();
+$table->addCell(3000)->addText('Подпись___________________',$textStyle);
+
 
 
 //$document->saveAs($outputFile);
